b447474b8e6444ce0b8a37a4410c0ff44c729a73
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-12-12 21:54:46 +0530

    Open up evaluate function

--- src/evaluation.ts
+++ src/evaluation.ts
@@ -1,5 +1,17 @@
 import { Grid } from './definitions';
 
 export function evaluate(grid: Grid): number {
+  return evaluateRows(grid) + evaluateColumns(grid) + evaluateDiagonals(grid);
+}
+
+function evaluateRows(grid: Grid): number {
+  return 0;
+}
+
+function evaluateColumns(grid: Grid): number {
+  return 0;
+}
+
+function evaluateDiagonals(grid: Grid): number {
   return 0;
 }
