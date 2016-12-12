a9c050cf8318e0f477071c71023cd6cf38a73aca
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-12-12 22:29:33 +0530

    Implement evaluateRows, evaluateColumns and evaluateDiagonals

--- src/evaluation.ts
+++ src/evaluation.ts
@@ -1,17 +1,28 @@
-import { Grid } from './definitions';
+import { Grid, Row, Column, Diagonal } from './definitions';
+import { getRows, getColumns, getDiagonals } from './utils';
 
 export function evaluate(grid: Grid): number {
   return evaluateRows(grid) + evaluateColumns(grid) + evaluateDiagonals(grid);
 }
 
 function evaluateRows(grid: Grid): number {
-  return 0;
+  return getRows(grid)
+    .map(evaluateCells)
+    .reduce((x, y) => x + y, 0);
 }
 
 function evaluateColumns(grid: Grid): number {
-  return 0;
+  return getColumns(grid)
+    .map(evaluateCells)
+    .reduce((x, y) => x + y, 0);
 }
 
 function evaluateDiagonals(grid: Grid): number {
+  return getDiagonals(grid)
+    .map(evaluateCells)
+    .reduce((x, y) => x + y, 0);
+}
+
+function evaluateCells(cells: boolean[]): number {
   return 0;
 }
