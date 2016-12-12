0dafddccdb0b29118b89dcd8b195b33f799d12e2
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-12-12 21:11:24 +0530

    Rewrite getMoves function declaratively

--- src/moves.ts
+++ src/moves.ts
@@ -1,8 +1,5 @@
 export function getMoves(grid: boolean[]): number[] {
-  const moves = [];
-  for(let i=0; i< grid.length; i++) {
-    if(grid[i] == undefined)
-      moves.push(i);
-  }
-  return moves;
+  return grid
+          .map((value, index) => value == undefined ? index : undefined)
+          .filter(value => value != undefined);
 }
