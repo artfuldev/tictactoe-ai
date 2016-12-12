d87c555f6973c86c77ab55ad137dcdeead3adb4d
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-12-12 21:01:52 +0530

    Search only for available moves

--- src/search.ts
+++ src/search.ts
@@ -1,5 +1,9 @@
+import { getMoves } from './moves';
+
 export function getBestMove(grid: boolean[], forX: boolean, depth?: number): number {
   if (depth == undefined)
-    depth = Math.sqrt(grid.length);
-  return Math.floor(Math.random() * grid.length);
+    depth = grid.length;
+  var moves = getMoves(grid);
+  // Return a random available move
+  return moves[Math.floor(Math.random() * moves.length)];
 }
