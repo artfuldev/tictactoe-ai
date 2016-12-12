1669c313c6faaabc01bec52d5768b9ca53e14094
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-12-12 21:50:52 +0530

    Make makeMove function use Grid and Move

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,5 @@
-export function makeMove(grid: boolean[], move: number, forX: boolean): boolean[] {
+import { Grid, Move } from './definitions';
+
+export function makeMove(grid: Grid, move: Move, forX: boolean): Grid {
   return grid.map((value, index) => index == move ? forX : value);
 }
