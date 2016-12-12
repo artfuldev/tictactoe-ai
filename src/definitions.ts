b2315d8353239dc147e5a45bd443244f0cfd0ec5
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-12-12 21:55:43 +0530

    Add additional interfaces

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,2 +1,5 @@
 export interface Grid extends Array<boolean> {};
+export interface Row extends Array<boolean> {};
+export interface Column extends Array<boolean> {};
+export interface Diagonal extends Array<boolean> {};
 export interface Move extends Number {};
