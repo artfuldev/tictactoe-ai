995ebc15d332e5770a7f2a0189c75a4f715b0a3c
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-12-12 21:18:39 +0530

    Use evaluation and move making in search

--- src/search.ts
+++ src/search.ts
@@ -1,9 +1,21 @@
 import { getMoves } from './moves';
+import { evaluate } from './evaluation';
+import { makeMove } from './game';
 
 export function getBestMove(grid: boolean[], forX: boolean, depth?: number): number {
   if (depth == undefined)
     depth = grid.length;
-  var moves = getMoves(grid);
-  // Return a random available move
-  return moves[Math.floor(Math.random() * moves.length)];
+  const moves = getMoves(grid);
+  const movesWithScores = moves.map(move => {
+    const newGrid = makeMove(grid, move, forX);
+    const evaluation = evaluate(newGrid);
+    return {
+      move,
+      score: forX ? evaluation : -evaluation
+    }; 
+  });
+  const sortedMovesWithScores = movesWithScores.sort((x, y) => x.score - y.score);
+  const sortedMoves = sortedMovesWithScores.map(x => x.move);
+  // Return the move with the best evaluation so far
+  return sortedMoves[0];
 }
