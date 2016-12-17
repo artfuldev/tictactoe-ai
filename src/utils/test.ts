69bbe228b09e6d94ad1e5c644aaa5b0e63e404ba
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-12-18 00:08:47 +0530

    Add tests for getRows

--- src/utils/test.ts
+++ src/utils/test.ts
@@ -1,6 +1,26 @@
-import { transpose } from './';
+import { getRows, transpose } from './';
 import { expect } from 'chai';
 import 'mocha';
+
+describe('getRows', () => {
+  it('should get n rows from nxn grid', () => {
+    const grid = [1,2,3,4,5,6,7,8,9];
+    const rows = getRows(grid);
+    expect(rows.length).to.equal(3);
+  });
+  it('should get rows from nxn grid each with length n', () => {
+    const grid = [1,2,3,4,5,6,7,8,9];
+    const rows = getRows(grid);
+    rows.forEach(row => expect(row.length).to.equal(3));
+  });
+  it('should get rows from nxn grid with proper values', () => {
+    const grid = [1,2,3,4,5,6,7,8,9];
+    const rows = getRows(grid);
+    expect(rows[0].join(',')).to.equal('1,2,3');
+    expect(rows[1].join(',')).to.equal('4,5,6');
+    expect(rows[2].join(',')).to.equal('7,8,9');
+  });
+});
 
 describe('transpose', () => {
   it('should transpose 3x3 grid properly', () => {
