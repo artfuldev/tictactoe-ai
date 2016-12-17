8f92302ebee41f12f0a5d10a81178379a433b439
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-12-17 23:34:34 +0530

    Add mutation test

--- src/utils/test.ts
+++ src/utils/test.ts
@@ -8,4 +8,9 @@ describe('transpose', () => {
     const transposed = transpose(grid);
     expect(transposed.join(',')).to.equal('1,4,7,2,5,8,3,6,9');
   });
+  it('should not mutate input grid', () => {
+    const grid = [1,2,3,4,5,6,7,8,9];
+    const transposed = transpose(grid);
+    expect(grid.join(',')).to.equal('1,2,3,4,5,6,7,8,9');
+  });
 });
