b6b79e31ebde6be07fea981cad9f8b00224fd894
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-12-17 23:33:04 +0530

    Wrote tests for transpose

---
+++ src/utils/test.ts
@@ -0,0 +1,11 @@
+import { transpose } from './';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('transpose', () => {
+  it('should transpose 3x3 grid properly', () => {
+    const grid = [1,2,3,4,5,6,7,8,9];
+    const transposed = transpose(grid);
+    expect(transposed.join(',')).to.equal('1,4,7,2,5,8,3,6,9');
+  });
+});
