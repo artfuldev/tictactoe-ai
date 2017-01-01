f7236371ccda5f89794b3f1e72ee6ac0b186fc90
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-01 22:48:06 +0530

    Change makeMove signature
    To take a function to determine who is to play next

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,6 @@
 import { Grid, Move } from './definitions';
 
-export function makeMove(grid: Grid, move: Move, forX: boolean): Grid {
-  return grid.map((value, index) => index == move ? forX : value);
+export function makeMove(grid: Grid, move: Move, forX: (grid: Grid) => boolean): Grid {
+  const newValue = forX(grid);
+  return grid.map((value, index) => index == move ? newValue : value);
 }
