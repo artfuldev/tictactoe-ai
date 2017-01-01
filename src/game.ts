f2c5215a6aea3e04e95a509a13a16cbcefa71c62
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-01 22:48:34 +0530

    Add hasGameEnded

--- src/game.ts
+++ src/game.ts
@@ -4,3 +4,8 @@ export function makeMove(grid: Grid, move: Move, forX: (grid: Grid) => boolean):
   const newValue = forX(grid);
   return grid.map((value, index) => index == move ? newValue : value);
 }
+
+export function hasGameEnded(grid: Grid): boolean {
+  // TODO: complete
+  return false;
+}
