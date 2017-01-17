3f9a388aee46f993d86681c44d419d1fb35f2f16
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-17 23:05:35 +0530

    Use minimax

--- src/search/index.ts
+++ src/search/index.ts
@@ -3,25 +3,23 @@ import { evaluate } from '../evaluation';
 import { nextValue, makeMove, hasGameEnded } from '../game';
 import { Grid, Move } from '../definitions';
 import { alphaBeta } from './alphaBeta';
+import { minimax } from './minimax';
 
 export function getBestMove(grid: Grid, forX?: (grid: Grid) => boolean,
   depth?: number): Move {
   const moves = getMoves(grid);
   if (depth == undefined) depth = moves.length;
-  if(forX == undefined) forX = nextValue;
+  if (forX == undefined) forX = nextValue;
   const isX = forX(grid);
   const movesWithScores = moves.map(move => {
     const newGrid = makeMove(grid, move, forX);
-    const evaluation = alphaBeta(newGrid, depth, -Infinity, Infinity, !isX,
-      evaluate, hasGameEnded,
-      grid => getMoves(grid).map(move => makeMove(grid, move, forX)));
+    const score = minimax(newGrid, depth, !isX, evaluate, hasGameEnded, grid => getMoves(grid).map(move => makeMove(grid, move, forX)));
     return {
       move,
-      score: isX ? evaluation : -evaluation
+      score: isX ? score: -score
     };
   });
-  const sortedMovesWithScores =
-    movesWithScores.sort((a, b) => b.score - a.score);
+  const sortedMovesWithScores = movesWithScores.sort((a, b) => b.score - a.score);
   const sortedMoves = sortedMovesWithScores.map(x => x.move);
   // Return the move with the best evaluation so far
   return sortedMoves[0];
