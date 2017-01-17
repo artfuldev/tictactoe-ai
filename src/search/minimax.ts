435748f1fac92542f0ee1ee9821de3339a544123
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-17 23:05:13 +0530

    Add minimax

---
+++ src/search/minimax.ts
@@ -0,0 +1,17 @@
+export function minimax<TNode>(
+  node: TNode,
+  depth: number,
+  maximizingPlayer: boolean,
+  evaluate: (n: TNode) => number,
+  isTerminalNode: (n: TNode) => boolean,
+  getChildren: (n: TNode) => TNode[]): number {
+  if (depth == 0 || isTerminalNode(node)) return evaluate(node);
+  if (maximizingPlayer)
+    return getChildren(node)
+      .reduce((best, child) => Math.max(best, minimax(child, depth - 1, false, evaluate, isTerminalNode, getChildren)),
+      -Infinity);
+  return getChildren(node)
+      .reduce((best, child) => Math.min(best, minimax(child, depth - 1, true, evaluate, isTerminalNode, getChildren)),
+      Infinity);
+  }
+    
