6bfe669a406f9d85f338a1bb2154c9d5e4040962
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2018-10-03 08:38:19 +0530

    Use types instead of interface extensions

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,6 +1,6 @@
-export interface Cell extends Boolean {};
-export interface Grid extends Array<Cell> {};
-export interface Row extends Array<Cell> {};
-export interface Column extends Array<Cell> {};
-export interface Diagonal extends Array<Cell> {};
-export interface Move extends Number {};
+export type Cell = Boolean;
+export type Grid = Cell[];
+export type Row = Cell[];
+export type Column = Cell[];
+export type Diagonal = Cell[];
+export type Move = Number;
