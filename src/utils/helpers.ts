e55391d215521c87cd0e8312339233f597271473
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2018-10-03 08:53:46 +0530

    Simplify utils

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,4 @@
-export function transpose<T>(grid: Array<T>): Array<T> {
+export const transpose = <T>(grid: T[]): T[] => {
   const size = Math.sqrt(grid.length);
   return grid.map((x, i) => grid[Math.floor(i / size) + ((i % size) * size)]);
-}
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,25 +1,22 @@
 import { transpose } from './helpers';
 
-function getArray(length: number): number[] {
-  return Array.apply(null, { length }).map(Number.call, Number);
-}
+const getArray = (length: number): number[] =>
+  Array.apply(null, { length }).map(Number.call, Number);
 
-export function getRows<T>(grid: T[]): T[][] {
+export const getRows = <T>(grid: T[]): T[][] => {
   const size = Math.sqrt(grid.length);
   const copy = grid.concat([]);
   return getArray(size).map(() => copy.splice(0, size));
-}
+};
 
-export function getColumns<T>(grid: T[]): T[][] {
-  return getRows(transpose(grid));
-}
+export const getColumns = <T>(grid: T[]): T[][] => getRows(transpose(grid));
 
-export function getDiagonals<T>(grid: T[]): T[][] {
+export const getDiagonals = <T>(grid: T[]): T[][] => {
   const size = Math.sqrt(grid.length);
   const lesser = size - 1;
   const last = grid.length - 1;
   return [
-    grid.filter((x, i) => Math.floor(i / size) === i % size),
-    grid.filter((x, i) => i > 0 && i < last && i % lesser === 0)
+    grid.filter((_, i) => Math.floor(i / size) === i % size),
+    grid.filter((_, i) => i > 0 && i < last && i % lesser === 0)
   ];
-}
+};
